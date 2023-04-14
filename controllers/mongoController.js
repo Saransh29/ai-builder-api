b134fc41f1cf88dd1c4bf8001e8ed2c0490b35fe
Saransh29 <saranshbibiyan234@gmail.com>
2023-04-15 00:47:55 +0530

    testing new server side retry logic

--- controllers/mongoController.js
+++ controllers/mongoController.js
@@ -1,5 +1,48 @@
 const Post = require("../mongo/post");
 
+const retry = async (fn, maxRetries, delayMs) => {
+  for (let attempt = 1; attempt <= maxRetries; attempt++) {
+    try {
+      return await fn();
+    } catch (err) {
+      if (attempt === maxRetries) {
+        throw err;
+      }
+      await new Promise((resolve) => setTimeout(resolve, delayMs));
+    }
+  }
+};
+
+const createPostWithRetry = async (req) => {
+  const { prompt, html, css, js, author } = req.body;
+  console.log("---Uploading---", prompt);
+
+  const newPost = await Post.create({
+    prompt,
+    html,
+    css,
+    js,
+    author,
+  });
+
+  return newPost;
+};
+
+exports.createPost = async (req, res) => {
+  try {
+    const newPost = await retry(() => createPostWithRetry(req), 3, 1000);
+    res.status(200).json({ success: true, data: newPost });
+  } catch (err) {
+    const { prompt } = req.body;
+    console.log("---failed to upload---", prompt);
+    console.error(err);
+    res.status(500).json({
+      success: false,
+      message: "Unable to create a post, please try again",
+    });
+  }
+};
+/*
 exports.createPost = async (req, res) => {
   try {
     const { prompt, html, css, js, author } = req.body;
@@ -17,12 +60,14 @@ exports.createPost = async (req, res) => {
   } catch (err) {
     const { prompt } = req.body;
     console.log("---failed to upload---", prompt);
+    console.error(err);
     res.status(500).json({
       success: false,
       message: "Unable to create a post, please try again",
     });
   }
 };
+*/
 
 exports.updatePost = async (req, res) => {
   const { id } = req.params;
